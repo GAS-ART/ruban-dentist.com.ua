65ddcbe9b6bf4e5d827fdf92d49544ed0a952262
Artem <temoha1386@gmail.com>
2022-09-06 12:42:44 +0300

    Added mobile images

--- resources/views/therapeutic.blade.php
+++ resources/views/therapeutic.blade.php
@@ -15,9 +15,9 @@
       <div class="service-main-screen__title">
          <h1 class="_animate">@lang('therapeutic.main_screen.title')</h1>
          <picture class="_animate">
-            <source type="image/webp" srcset="{{asset('/img/therapeutic/main.webp')}}">
-            <source srcset="{{asset('/img/therapeutic/main.jpg')}}">
-            <img src="{{asset('/img/therapeutic/main.jpg')}}" alt="@lang('therapeutic.main_screen.alt')">
+            <source type="image/webp" srcset="{{asset('/img/therapeutic/main_mob.webp')}}">
+            <source srcset="{{asset('/img/therapeutic/main_mob.jpg')}}">
+            <img src="{{asset('/img/therapeutic/main_mob.jpg')}}" alt="@lang('therapeutic.main_screen.alt')">
          </picture>
          <h3 class="_animate">@lang('therapeutic.main_screen.text')</h3>
          <a href="tel:%2B0956664748" class="_animate">@lang('therapeutic.main_screen.call')</a>
